c8cdacbc169dd5254ba0a95566cc9413e00b9aaa
Tyler Dillon <harrell_t@icloud.com>
2019-06-18 13:41:31 -0400

    Added directional functionality to modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,120 @@
+let currInd = 0;
+let dataLength;
+
+//Function to convert state name to 2 digit abbreviation
+//adapted from https://stackoverflow.com/questions/3925195/making-state-abbreviations-from-state-names 
+const abbrev = (name) => {
+    switch (name.toUpperCase())
+    {
+        case "ALABAMA":
+            return "AL";
+        case "ALASKA":
+            return "AK";
+        case "ARIZONA":
+            return "AZ";
+        case "ARKANSAS":
+            return "AR";
+        case "CALIFORNIA":
+            return "CA";
+        case "COLORADO":
+            return "CO";
+        case "CONNECTICUT":
+            return "CT";
+        case "DELAWARE":
+            return "DE";
+        case "DISTRICT OF COLUMBIA":
+            return "DC";
+        case "FLORIDA":
+            return "FL";
+        case "GEORGIA":
+            return "GA";
+        case "HAWAII":
+            return "HI";
+        case "IDAHO":
+            return "ID";
+        case "ILLINOIS":
+            return "IL";
+        case "INDIANA":
+            return "IN";
+        case "IOWA":
+            return "IA";
+        case "KANSAS":
+            return "KS";
+        case "KENTUCKY":
+            return "KY";
+        case "LOUISIANA":
+            return "LA";
+        case "MAINE":
+            return "ME";
+        case "MARYLAND":
+            return "MD";
+        case "MASSACHUSETTS":
+            return "MA";
+        case "MICHIGAN":
+            return "MI";
+        case "MINNESOTA":
+            return "MN";
+        case "MISSISSIPPI":
+            return "MS";
+        case "MISSOURI":
+            return "MO";
+        case "MONTANA":
+            return "MT";
+        case "NEBRASKA":
+            return "NE";
+        case "NEVADA":
+            return "NV";
+        case "NEW HAMPSHIRE":
+            return "NH";
+        case "NEW JERSEY":
+            return "NJ";
+        case "NEW MEXICO":
+            return "NM";
+        case "NEW YORK":
+            return "NY";
+        case "NORTH CAROLINA":
+            return "NC";
+        case "NORTH DAKOTA":
+            return "ND";
+        case "OHIO":
+            return "OH";
+        case "OKLAHOMA":
+            return "OK";
+        case "OREGON":
+            return "OR";
+        case "PENNSYLVANIA":
+            return "PA";
+        case "PUERTO RICO":
+            return "PR";
+        case "RHODE ISLAND":
+            return "RI";
+        case "SOUTH CAROLINA":
+            return "SC";
+        case "SOUTH DAKOTA":
+            return "SD";
+        case "TENNESSEE":
+            return "TN";
+        case "TEXAS":
+            return "TX";
+        case "UTAH":
+            return "UT";
+        case "VERMONT":
+            return "VT";
+        case "VIRGIN ISLANDS":
+            return "VI";
+        case "VIRGINIA":
+            return "VA";
+        case "WASHINGTON":
+            return "WA";
+        case "WEST VIRGINIA":
+            return "WV";
+        case "WISCONSIN":
+            return "WI";
+        case "WYOMING":
+            return "WY";
+    }
+}
+
 //Function to create the gallery of cards
 const populateGallery = (users) => {
     for (let i = 0; i < users.length; i++) {
@@ -20,7 +137,7 @@ const populateGallery = (users) => {
 const generateModals = (users) => {
     for (let i = 0; i < users.length; i++) {
         let date = new Date(users[i].dob.date);
-        let phone = users[i].cell
+        let phone = users[i].cell;
         $('body').append(
             `<div class="modal-container" id=modal${i}>
                 <div class="modal">
@@ -32,8 +149,10 @@ const generateModals = (users) => {
                         <p class="modal-text cap">${users[i].location.city}</p>
                         <hr>
                         <p class="modal-text">${phone}</p>
-                        <p class="modal-text">${users[i].location.street}, ${users[i].location.city}, ${users[i].location.state} ${users[i].location.postcode}</p>
+                        <p class="modal-text cap">${users[i].location.street}, ${users[i].location.city}, ${abbrev(users[i].location.state)} ${users[i].location.postcode}</p>
                         <p class="modal-text">Birthday: ${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}</p>
+                        <button type="button" id="decrement"><strong><</strong></button>  
+                        <button type="button" id="increment"><strong>></strong></button>
                     </div>
                 </div>
             </div>`
@@ -43,11 +162,12 @@ const generateModals = (users) => {
 
 //Function to run the API call to generate the users
 const generateUsers = (url) => {
-    //API request: grabs 12 random users
+    //API request: grabs random users
     fetch(url)
         .then(res => res.json())
         .then(data => {
             let userData = data.results;
+            dataLength = userData.length;
             //generates the cards
             populateGallery(userData);
             //generates and hides the modals
@@ -57,8 +177,30 @@ const generateUsers = (url) => {
 };//end of generateUsers function
 
 //runs the API call
-generateUsers('https://randomuser.me/api/?results=12');
+generateUsers('https://randomuser.me/api/?results=12&nat=us');
 
+
+//Increment and Decrement functions for the modals
+const increment = () => {
+    $('.modal-container').hide();
+    currInd = parseInt(currInd)
+    if (currInd === (dataLength - 1)) {
+        currInd = 0;
+    } else {
+      currInd += 1;
+    };
+    $(`#modal${currInd}`).show();
+};
+const decrement = () => {
+    $('.modal-container').hide();
+    currInd = parseInt(currInd)
+    if (currInd === 0) {
+        currInd = dataLength - 1;
+      } else {
+        currInd -= 1;
+      }
+    $(`#modal${currInd}`).show();
+}
 
 //Event listeners
 //Because elements were added dynamically, needed to target the document rather than the individual elements
@@ -67,8 +209,11 @@ generateUsers('https://randomuser.me/api/?results=12');
 //Targets the cards. When clicked, shows modal
 $(document).on('click', '.card', (e) => {
         let i = $(e.currentTarget).attr('id');
+        currInd = i;
         $(`#modal${i}`).show();
 })
 
-//Target the 'X' in the top right of the modal. When clicked, hides the modal
+//Modal event listeners
 $(document).on('click', '#modal-close-btn', () => {$('.modal-container').hide()})
+$(document).on('click', '#increment', increment)
+$(document).on('click', '#decrement', decrement)
